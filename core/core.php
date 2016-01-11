ddd1df962bd517e7a4be92c159dbc13313af3d97
Brayan Narváez <princk093@gmail.com>
2016-01-11 12:29:08 -0430

    Video tres
    Estructura de la app y librerias con composer

---
+++ core/core.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  EL NÚCLEO DE LA APLICACIÓN!
+*/
+
+define('HTML_DIR','html/');
+define('APP_TITLE','OcrendBB');
+define('APP_COPY','Copyright &copy; ' . date('Y',time()) . ' Ocrend Software.');
+
+require('vendor/autoload.php');
+
+?>
